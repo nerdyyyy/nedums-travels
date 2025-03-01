2ff11df3683c95c45c05e618bd2850c7087a568c
nerdyyyy <obanyechinedu@gmail.com>
2025-03-01 18:19:20 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,26 @@
+let slideIndex = 0;
+
+function showSlides() {
+    let slides = document.getElementsByClassName("slide");
+    for (let i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    slideIndex++;
+    if (slideIndex > slides.length) { slideIndex = 1; }
+    slides[slideIndex - 1].style.display = "block";
+    setTimeout(showSlides, 5000); // Change slide every 5 seconds
+}
+
+function moveSlide(n) {
+    let slides = document.getElementsByClassName("slide");
+    slideIndex += n;
+    if (slideIndex > slides.length) { slideIndex = 1; }
+    if (slideIndex < 1) { slideIndex = slides.length; }
+    for (let i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    slides[slideIndex - 1].style.display = "block";
+}
+
+// Start the slideshow
+showSlides();
